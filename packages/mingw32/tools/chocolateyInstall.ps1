78f805a8ea67b8acccdbc6924399c5192dc3d3e6
mikee47 <mike@sillyhouse.net>
2020-12-25 20:58:46 +0000

    fix installation package names

--- packages/mingw32/tools/chocolateyInstall.ps1
+++ packages/mingw32/tools/chocolateyInstall.ps1
@@ -3,6 +3,6 @@ $url='https://github.com/SmingHub/SmingTools/releases/download/1.0/'
 
 $mingwInstallDir = "$($env:SystemDrive)\MinGW"
 Install-ChocolateyPath "$mingwInstallDir\bin;$mingwInstallDir\msys\1.0\bin" 'Machine'
-Install-ChocolateyZipPackage "$packageName.MinGW32" "$url/MinGW-2020-10-19.7z" "$mingwInstallDir\.."
+Install-ChocolateyZipPackage "$packageName" "$url/MinGW-2020-10-19.7z" "$mingwInstallDir\.."
 
 Update-SessionEnvironment
