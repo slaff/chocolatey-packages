5dc57b2d03b35233c047663eed98d541d7c5e39c
Mike <mike@sillyhouse.net>
2021-02-09 17:18:46 +0000

    Add sming.esp32 package (#5)
    * Correct sming.source .nuspec filename
    
    * Add CMake to path
    
    * Esp32 installation

--- packages/sming/tools/chocolateyInstall.ps1
+++ packages/sming/tools/chocolateyInstall.ps1
@@ -4,4 +4,8 @@ $python = "C:\Python39\python"
 Install-ChocolateyEnvironmentVariable "PYTHON" "$python" 'Machine'
 Write-Debug "Set PYTHON to $python"
 
+$cmake = "$env:ProgramFiles\CMake\bin"
+Install-ChocolateyPath "$cmake" 'Machine'
+Write-Debug "Add $cmake to system PATH"
+
 Update-SessionEnvironment
